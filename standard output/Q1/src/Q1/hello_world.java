a8bf15b6e68b6694422a5951c17d08e0fd5ebca0
isaiah im <isaiahim0214@gmail.com>
2022-12-28 21:14:48 +0900

    1. 기본 골격 및 표준 출력

---
+++ standard output/Q1/src/Q1/hello_world.java
@@ -0,0 +1,7 @@
+package Q1;
+
+public class hello_world {
+	public static void main(String[] args) {
+		System.out.println(10+25);
+	}
+}
---
+++ standard output/Q3/src/Q3/hello_world.java
@@ -0,0 +1,7 @@
+package Q3;
+
+public class hello_world {
+	public static void main(String[] args) {
+		System.out.println(13*21);
+	}
+}
