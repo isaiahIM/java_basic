2ae65ba4b7b1312b539098f62d4116baed057b97
isaiah im <isaiahim0214@gmail.com>
2023-01-13 00:00:02 +0900

    3. 상수와 형 변환

---
+++ constant and casting/Q1/src/q1/Q1.java
@@ -0,0 +1,13 @@
+package q1;
+
+public class Q1 {
+	public static void main(String[] args){
+		int num=10;
+		float pi=3.14159f;
+		int result;
+		
+		result=num+(int)pi;
+		
+		System.out.println(result);
+	}
+}
---
+++ constant and casting/Q2/src/q2/Q2.java
@@ -0,0 +1,13 @@
+package q2;
+
+public class Q2 {
+	public static void main(String[] args){
+		int hexData=0x34;
+		float floatData=5.12f;
+		float result;
+		
+		result=hexData+floatData;
+		
+		System.out.println(result);
+	}
+}
